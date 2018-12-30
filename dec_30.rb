54d6654ccde2c0823625f42689157f928cf86459
‘nathashas1’ <nathasha36@gmail.com>
2018-12-30 11:57:45 -0800

    dec 30

--- dec_30.rb
+++ dec_30.rb
@@ -12,34 +12,32 @@ class BinaryMinheap
   end
 
   def self.heapify_up(array, child_idx, len, &prc)
-    prc ||= Proc.new { |a,b| a<=>b }
     return array if child_idx == 0
     parent_idx = (child_idx-1)/2
     parent_val = array[parent_idx]
     child_val = array[child_idx]
     if prc.call(child_val, parent_val) < 0
-      array[child_idx], array[parent_idx] = array[parent_idx], array[child_idx]
+      array[parent_idx], array[child_idx] =   array[child_idx], array[parent_idx]
       self.heapify_up(array, parent_idx, len, &prc)
     end
     array
   end
 
   def extract
-    @store[@store.length - 1], @store[0] = @store[0], @store[@store.length - 1]
+    @store[0], @store[@store.length-1] = @store[@store.length-1],@store[0]
     val = @store.pop
-    BinaryMinheap.heapify_down(@store, 0 , @store.length, &@prc)
+    BinaryMinheap.heapify_down(@store, 0, @store.length, &@prc)
     val
   end
 
   def self.child_indices(parent_idx, len)
-    indices = [2*parent_idx + 1, 2* parent_idx+2]
+    indices = [2*parent_idx+1, 2*parent_idx+2]
     indices.pop if indices.last >= len
     indices.pop if indices.last >= len
     indices
   end
 
-  def heapify_down(array, parent_idx, len, &prc)
-    prc ||= Proc.new { |a,b| a<=>b }
+  def self.heapify_down(array, parent_idx, len, &prc)
     idxs = self.child_indices(parent_idx, len)
     return array if idxs.empty?
     if idxs.length == 1
@@ -51,13 +49,35 @@ class BinaryMinheap
         smallest_idx = idxs[1]
       end
     end
-    smallest_child_val = array[smallest_idx]
     parent_val = array[parent_idx]
+    smallest_child_val = array[smallest_idx]
     if prc.call(smallest_child_val, parent_val) < 0
       array[smallest_idx], array[parent_idx] = array[parent_idx], array[smallest_idx]
       self.heapify_down(array, smallest_idx, len, &prc)
     end
     array
   end
-
 end
+
+class Array
+  def merge_sort
+    return self if self.length  == 1
+    mid  = self.length/2
+    left  = self[0...mid].merge_sort
+    right = self[mid..-1].merge_sort
+    merge(left, right)
+  end
+
+  def merge(left, right, &prc)
+    prc ||= Proc.new { |a,b| a<=>b }
+    result = []
+    until left.empty? || right.empty?
+      if prc.call(left[0], right[0]) < 0
+        result << left.shift
+      else
+        result << right.shift
+      end
+    end
+    result+left+right
+    end
+  end
