94d1a6db8d3082493b3d346598bf83a939da1bff
‘nathashas1’ <nathasha36@gmail.com>
2019-01-02 16:13:19 -0800

    bst

--- jan_02.rb
+++ jan_02.rb
@@ -82,3 +82,98 @@ class Array
     result + left + right
   end
  end
+
+
+ class BSTNode
+   attr_reader :value
+   attr_accessor :left, :right
+
+   def initialize(value)
+     @value = value
+     @left = nil
+     @right = nil
+   end
+ end
+
+class BinarySearchTree
+  def initialize
+    @root = nil
+  end
+
+  def insert(value)
+    @root = insert_into_tree(@root, value)
+  end
+
+  def delete(value)
+    @root = delete_from_tree(@root, value)
+  end
+
+  def maximum(tree_node = @root)
+    if tree_node.right
+      maximum_node = maximum(tree_node.right)
+    else
+      maximum_node = tree_node
+    end
+    maximum_node
+  end
+
+
+  def insert_into_tree(tree_node, value)
+    return BSTNode.new(value) if tree_node.nil?
+    if value <= tree_node.value
+      tree_node.left = insert_into_tree(tree_node.left, value)
+    else
+      tree_node.right = insert_into_tree(tree_node.right, value)
+    end
+    tree_node
+  end
+
+  def delete_from_tree(tree_node, value)
+    if value == tree_node.value
+      tree_node = remove(tree_node)
+    elsif value < tree_node.value
+        tree_node.left = delete_from_tree(tree_node.left, value)
+    else
+        tree_node.right = delete_from_tree(tree_node.right, value)
+    end
+    tree_node
+  end
+
+  def remove(node)
+    if node.left.nil? && node.right.nil?
+      node = nil
+    elsif node.left.nil? && node.right
+      node = node.right
+    elsif node.left && node.right.nil?
+      node = node.left
+    else
+      node = replace_parent(node)
+    end
+    node
+  end
+
+  def replace_parent(node)
+    replacement_node = maximum(node.left)
+    if replacement_node.left
+      promote_child(node.left)
+    end
+    replacement_node.left = node.left
+    replacement_node.right = node.right
+    replacement_node
+  end
+
+  def promote_child(tree_node)
+    if tree_node.right
+      parent = tree_node
+      child = tree_node.right
+    while child.right
+      parent = child
+      child = child.right
+    end
+    parent.right = child.left
+  end
+  else
+    tree_node = tree_node.left
+  end
+
+end
