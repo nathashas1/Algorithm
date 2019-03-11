36c6d26b58c9ecba3ce32622d61075172cc035f7
nathashas1 <nathasha36@gmail.com>
2019-03-10 23:02:24 -0700

    bst

--- mar_10.rb
+++ mar_10.rb
@@ -81,3 +81,17 @@ class Array
     result+left+right
     end
   end
+
+  def merge(left, right, &prc)
+    prc ||= Proc.new { |a,b| a<=>b }
+    result = []
+    until left.empty? || right.empty?
+      if prc.call(left[0], right[0]) < 0
+        result << left.shift
+      else
+        result << right.shift
+      end
+    end
+    result+left+right
+    end
+  end
