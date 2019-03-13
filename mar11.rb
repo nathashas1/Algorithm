2cc1761b2768261a074afb0508f927d022b702f7
nathashas1 <nathasha36@gmail.com>
2019-03-12 23:08:37 -0700

    heap

--- mar11.rb
+++ mar11.rb
@@ -104,3 +104,35 @@ class BinaryMinheap
       right = self[mid..-1].merge_sort
       merge(left, right)
     end
+
+
+    @store.push(val)
+    BinaryMinheap.heapify_up(@store, @store.length-1, @store.length, &@prc)
+  end
+
+
+  def self.heapify_up(array, child_idx, len, &prc)
+    prc ||= Proc.new{|a,b| a<=>b}
+    return array if child_idx == 0
+    parent_idx = (child_idx-1)/2
+    parent_val, child_val = [array[parent_idx], array[child_idx]]
+    if prc.call(child_val, parent_val) < 0
+      array[child_idx], array[parent_idx] = array[parent_idx], array[child_idx]
+      self.heapify_up(array, parent_idx, len, &prc)
+    end
+    array
+  end
+
+  def extract
+    @store[0], @store[@store.length-1] = @store[@store.length - 1], @store[0]
+    val = @store.pop
+    BinaryMinheap.heapify_down(@store, 0, @store.length, &@prc)
+    val
+  end
+
+  def self.child_indices(parent_idx, len)
+    indices = [2*parent_idx+1, 2*parent_idx+2]
+    indices.pop if indices.last >= len
+    indices.pop if indices.last >= len
+    indices
+  end
